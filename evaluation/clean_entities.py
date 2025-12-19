c9c2fe67de562d2d5a37691b4e7a6a8e0f0de029
madsam999 <samuelfournier06@gmail.com>
2025-12-19 10:30:59 -0500

    Clean

---
+++ evaluation/clean_entities.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+
+DIRECTORY_OF_RESULTS = "results"
+DIRECTORY_OF_CSV = "csv"
+PATH_TO_RELATIONS = os.path.join(DIRECTORY_OF_RESULTS, DIRECTORY_OF_CSV, "entities.csv")
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    entities_df = pd.read_csv(PATH_TO_RELATIONS, sep = ";")
+
+    unk_mask = entities_df["entity"].str.lower().isin(["unk", "unkown", "none"])
+
+    total_unk_count = entities_df.loc[unk_mask, "frequency"].sum()
+    total_entity_count = entities_df["frequency"].sum()
+
+    if total_entity_count > 0:
+        percentage = (total_unk_count / total_entity_count) * 100
+    else:
+        percentage = 0
+    
+    print(f"Total Entities Found: {total_entity_count}")
+    print(f"Unknown Entities Found: {total_unk_count}")
+    print(f"Noise Percentage: {percentage:.2f}%")
+
+    df_cleaned = entities_df[~unk_mask]
+
+    df_cleaned.to_csv(os.path.join(DIRECTORY_OF_RESULTS, DIRECTORY_OF_CSV, "entities_clean.csv"))
---
+++ evaluation/clean_relations.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+DIRECTORY_OF_RESULTS = "results"
+DIRECTORY_OF_CSV = "csv"
+PATH_TO_RELATIONS = os.path.join(DIRECTORY_OF_RESULTS, DIRECTORY_OF_CSV, "relations.csv")
+
+def main():
+    relations_df = pd.read_csv(PATH_TO_RELATIONS, sep = ";")
+
+    totalRows = len(relations_df)
+    duplicateRows = relations_df.duplicated().sum()
+    percentage = (duplicateRows / totalRows) * 100
+
+    print(f"Total Rows: {totalRows}")
+    print(f"Duplicate Count: {duplicateRows}")
+    print(f"Duplicate Percentage: {percentage}")
+
+    relations_df.drop_duplicates(inplace = True)
+
+    relations_df.to_csv("results/csv/relations_clean.csv", sep = ";", index = False)
+
+
+if __name__ == "__main__":
+    main()
